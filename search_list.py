53e2d0376fa99f860b7b73601fac72b54cf59678
LPouliot <113251856+LPouliot@users.noreply.github.com>
2023-10-13 16:31:01 -0400

    search_list.py

--- search_list.py
+++ search_list.py
@@ -1,25 +1,25 @@
-# Searching for certain indexes in a list
-
-letters = ['a', 'w', 'q', 'a', 's', 'd', 'c', 'z', 's']
-# Develop coe that will allow a user to input a letter. Search for the letter. Return its index position
+# Develop code that will allow a user to input a letter. Search for the letter. Return its index position
 # and how many times it was found in the list. if it is not in the list, let the user know it was not
 # found in the list. Ask if the user would like to add it to the list. If yes, add it. If no, end.
+letters = ['a', 'w', 'q', 'a', 's', 'd', 'c', 'z', 's']
 
-guess = (input("Guess a letter in our list:" ))
-# guess = 'a'
-def searchlist():
+guess = (input("Guess a letter in our list: "))
+def searchlist(guess):
     count = 0
     for i in letters:
         if(i == guess):
             count = count + 1
         else:
             continue
-    print(count)
     if(count > 0):
         print("The letter", guess, "was found", count,"times")
+        print("The index position(s) of the letter are located at:")
+        for idx, i in enumerate(letters):
+            if i == guess:
+                print(idx)
     elif(count == 0):
-        print("The letter was not found")
-    else:
         print("This letter was not found in our list")
+    else:
+        print("The letter was not found")
+searchlist(guess)
 
-searchlist()
