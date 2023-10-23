241390065a3a452e9c35f365b68910020fb8b1ae
LPouliot <113251856+LPouliot@users.noreply.github.com>
2023-10-23 12:05:16 -0400

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,22 @@
+# Working with f_strings
+
+import math
+# import replit
+
+p = math.pi
+
+name = "Jane"
+age = 25
+
+
+print ("Hello, {}! You're {} years old.".format(name,age))
+print("Hello, ",name + "! You're ",age,"years old")
+
+
+# The Modulo Operator, %
+
+name = "Jane"
+
+print("Hello, %s" % name)
+print("Hello,",name+"!")
+
